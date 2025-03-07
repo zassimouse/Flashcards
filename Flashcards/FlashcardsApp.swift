8bf496915ca85f0c3c8efb944983ee4218574cae
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2025-03-07 16:23:42 +0300

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Denis Haritonenko on 7.03.25.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
